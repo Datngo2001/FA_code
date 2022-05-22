1670b63310f7b12b5c9a4093c51f36dfcba6cd87
DatNgo <ngominhdat110115@gmail.com>
2022-05-22 21:18:49 +0700

    done all

--- FA_bt7/script.js
+++ FA_bt7/script.js
@@ -1,17 +1,36 @@
 let num1 = document.getElementById("num1")
 let num2 = document.getElementById("num2")
+let notify = document.getElementById("notify")
+
+document.getElementById("add").onclick = createCalCulateHandler("add")
+document.getElementById("sub").onclick = createCalCulateHandler("sub")
+document.getElementById("mul").onclick = createCalCulateHandler("mul")
+document.getElementById("div").onclick = createCalCulateHandler("div")
 
 function createCalCulateHandler(name) {
     return (e) => {
         if (name == "add") {
-            return num1 + num2
+            notify.innerHTML = convertToNum(num1.value) + convertToNum(num2.value)
         } else if (name == "sub") {
-            return num1 - num2
+            notify.innerHTML = convertToNum(num1.value) - convertToNum(num2.value)
         } else if (name == "mul") {
-            return num1 * num2
+            notify.innerHTML = convertToNum(num1.value) * convertToNum(num2.value)
         } else if (name == "div") {
-            return num1 / num2
+            debugger
+            let num2val = convertToNum(num2.value)
+            if (num2val == 0) {
+                notify.innerHTML = "Devided by 0"
+                return
+            }
+            notify.innerHTML = convertToNum(num1.value) / num2val
         }
-
     }
+}
+
+function convertToNum(num) {
+    result = parseFloat(num)
+    if (isNaN(num)) {
+        return 0;
+    }
+    return result
 }
