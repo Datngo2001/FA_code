61affd64f8fea58168806b48f575d52694be6974
datngo2001 <ngominhdat110115@gmail.com>
2022-07-09 18:04:53 +0700

    start day 6

--- FRD/Day05/demo-modal/src/App.js
+++ FRD/Day05/demo-modal/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Modal from './components/Modal/Modal';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  function openModal() {
+    setIsOpen(() => true)
+  }
+
+  function closeModal() {
+    setIsOpen(() => false)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={openModal} style={{ "marginTop": "100px" }}> Open modal </button>
+      {isOpen ? (
+        <Modal close={closeModal} title={"Login"}></Modal>
+      ) : <></>}
     </div>
   );
 }
