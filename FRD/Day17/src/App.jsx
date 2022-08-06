1448407229cd21ec5b2c86f6ee0dad6938241eb3
datngo2001 <ngominhdat110115@gmail.com>
2022-08-06 19:34:05 +0700

    config eslint

---
+++ FRD/Day17/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Avatar from "./Avatar";
+const App = () => {
+  return (
+    <h1>
+      <Avatar></Avatar>
+    </h1>
+  );
+};
+
+export default App;
---
+++ FRD/Day17/src/Avatar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import avatar from "./asset/dog";
+
+function Avatar() {
+  return (
+    <div>
+      <h1>Avatar</h1>
+      <img src={avatar} alt="" srcset="" />
+    </div>
+  );
+}
+
+export default Avatar;
